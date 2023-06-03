760e744555eb7daf7ba1324df140799e193b9f4a
Linda Vadysirisack <linda.vady@gmail.com>
2023-06-03 17:35:08 -0500

    updated lab four -added optional work in comments -  one left to solve

--- day_four/lab/labfour/indexfour.js
+++ day_four/lab/labfour/indexfour.js
@@ -582,3 +582,64 @@ function oldAndLoud(person) {
 }
 oldAndLoud(user);
 console.log(user.age,user.name);
+
+/*
+Requirements Complete!
+The remainder of this lab is provided as additional practice if you have the time. 
+If you have run out of time, the following is optional.
+
+H. Mama cat
+Define an object called cat1 that contains the following properties:
+
+name
+breed
+age (a number)
+console.log the cat's age and breed.
+
+I. Papa cat
+Define an object called cat2 that contains the same properties.
+
+J. Combine Cats!
+The cats are multiplying!
+
+Write a function combineCats that has two parameters: mama, and papa. The function will take two arguments -- each a cat object.
+Pass cat1 and cat2 as arguments to the combineCats function. The function should console.log them.
+Example:
+
+combineCats(cat1, cat2)
+{ name: "Joe", age: 19, breed: "Mog" } { name: "Jam", age: 45, breed: "Siamese" }
+
+This is to demonstrate that functions can take objects as arguments
+
+You could also invoke the combineCats function by writing the objects straight into the parentheses:
+
+combineCats({ name: "Craig", age: 20, breed: "unknown" }, { name: "Linda", age: 20, breed: "undefined" });
+Make it so the combineCats function will return a combination of the two incoming cats. The result should be an object wherein the:
+
+name is a concatenation of the parents' names.
+age is 1.
+breed is each of the parents' breeds with a hyphen in between.
+Example:
+
+console.log(combineCats(cat1, cat2));
+{ name: 'JoeJam', age: 1, breed: 'Mog-Siamese' }
+
+This is to demonstrate that a function can return an object
+
+K. Cat Brain-Bender
+If combineCats returns an object, and if combineCats takes objects as arguments, then it stands to reason that:
+
+combineCats can use itself as its own argument.
+
+Take a second to stew on that.
+
+What is the result of the following?
+
+console.log(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)));
+The above console.log is two levels deep of combineCats.
+
+Write a console.log that is three levels deep of combineCats. combineCats should have two arguments, each which are combineCats, each which have two arguments, each which are combineCats.
+Your output should look something like:
+
+{ name: 'JoeJamJoeJamJoeJamJoeJam`, age: 1, breed: 'Mog-Siamese-Mog-Siamese-Mog-Siamese-Mog-Siamese' }
+*/
